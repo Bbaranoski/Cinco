dca984afea47becc562cf29556b448ae1c1b702a
BBaranoski <breno.b.o.baranoski@gmail.com>
2025-08-05 14:03:50 -0300

    resolvendo problema de modulo e compilador, teste nas rotas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 import cors from 'cors';
-import { wordRouter } from '';
+import { wordRouter } from './routes/word.routes';
 
 const app: Application = express();
 const PORT: number = Number(process.env.PORT) || 3333;
@@ -9,4 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/words', wordRouter);
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));""
